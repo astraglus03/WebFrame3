3ae2883bc63b858f76e2d5deac818d9a8136bf36
astraglus <kkd0250@gmail.com>
2023-04-03 15:27:36 +0900

    post model produce and adjust time

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model): # 일종의 DB구조, field 설정하는 부분
+    title=models.CharField(max_length=30)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    #author
+
+    def __str__(this):
+        return f'[{this.pk}]{this.title}' # 이게f'' 장고에서 문자열임.
+
+
+
