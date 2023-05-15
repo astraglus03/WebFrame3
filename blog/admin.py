3bc2513944acfad0eae3a54702bcbba58fe66609
astraglus <kkd0250@gmail.com>
2023-05-15 15:41:52 +0900

    23.05.15

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post,Category
+from .models import Post,Category,Tag
 admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)} # 자동으로 채워주는 역할을 하는 field
 
 admin.site.register(Category,CategoryAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Tag,TagAdmin)
+
+
+# 전체 테스트 = python manage.py test
+# 특정 테스트= python manage.py test 파일명 ex) blog.test2category
+# 테스트 코드 수정하지말고 테스트 할 것.
+
--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+from django.shortcuts import render
+
+
 # Create your models here.
 
-class Category(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True) # allow unicode= 한글도 가능하게
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)  # allow unicode= 한글도 가능하게
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):  # DB table
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)  # allow unicode= 한글도 가능하게
+
     # slug: 주요단어 뽑아내는 것.(제목화 될수 있는것들) html코드로만 쓸수 있는거만 가능
 
     def __str__(self):
@@ -14,12 +25,14 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/category/{self.slug}'
+
     class Meta:
-        verbose_name_plural='Categories' # 테이블에 이름 강제 지정 가능함.
+        verbose_name_plural = 'Categories'  # 테이블에 이름 강제 지정 가능함.
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    hook_text=models.CharField(max_length=100,blank=True)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     # 이렇게 해야 서버에서 파일을 찾아오는 시간을 단축할 수 있음. blank=True <- 해당 필드는 필수 항목은 아니라는 뜻
@@ -34,8 +47,11 @@ class Post(models.Model):
     # null=True -> null값이 들어가도 된다는 의미 / on_delete=SET_NULL은 삭제된 정보는 null로 처리함 이때 null=True로 되어있어야함
     # on_delete=models.CASCADE # 정보 싹다 삭제
 
+    # category는 일대다 관계일때 사용했음
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
+    # tag는 다대다 관계임. -> ForeignKey가 아니라 ManyToManyField로 해야함
+    tags = models.ManyToManyField(Tag, blank=True)
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
@@ -44,7 +60,6 @@ class Post(models.Model):
 
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
+
     def get_file_ext(self):
-        return self.get_file_name().split('.')[-1] # -1은 마지막 인덱스 ex) txt / xlsx / docx
-
-
+        return self.get_file_name().split('.')[-1]  # -1은 마지막 인덱스 ex) txt / xlsx / docx
